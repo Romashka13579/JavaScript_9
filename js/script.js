9c74b323c5fdccc4357bd92b01b1fcb8faec5898
Romashka <rroma13579@gmail.com>
2022-08-29 17:07:11 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,2 @@
 var slider_1_full = document.getElementById("slider-1-full")
 var slider_2_2 = document.getElementById("slider-1-1")
-function Slider_1(a) {
-    console.log("ar");
-    switch (a) {
-        case "1":
-            slider_1_full.style.left = "-100vw"
-            slider_2_2.style.background = "url(../img/maxresdefault\ \(1\)\ 2.png)"
-            break
-        case "3":
-            slider_1_full.style.left = "-100vw"
-            slider_2_2.style.background = "url(../img/maxresdefault\ \(1\)\ 2.png)"
-            break
-    }
-}
